d086472fa61e1f2647787444e30007881b20acfd
Shreyaan <11375404+Shreyaan@users.noreply.github.com>
2024-09-07 18:01:02 +0530

    twillo

---
+++ backend/utils/twillo.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+import os
+
+
+def send_sms(to_number, message_text):
+    account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+    auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to=to_number,
+        from_=os.getenv("TWILIO_PHONE_NUMBER"),  # Add this line
+        body=message_text,
+    )
+
+    return message.sid
