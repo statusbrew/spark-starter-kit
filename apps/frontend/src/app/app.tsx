9656fa65717713ff392b224799570bf53f52048f
Harnoor Singh <harnoorsingharora2005@gmail.com>
2024-09-07 10:09:30 +0530

    frontend init

---
+++ apps/frontend/src/app/app.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>
+      hello world!
+    </div>
+  );
+}
+
+export default App;
