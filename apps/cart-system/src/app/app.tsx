1d9a96f6d413a9773ba08a275c8b0749bb5862b3
TusharGeek <tusharanand517@gmail.com>
2024-09-07 16:11:46 +0530

    Final add to cart

--- apps/cart-system/src/app/app.tsx
+++ apps/cart-system/src/app/app.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Cart from "./pages/Cart"; 
-import BarCode from "./pages/BarCode";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/cart" element={<Cart />} />
-        <Route path="/barcode" element={<BarCode />} />
         <Route path="*" element={<Navigate to="/cart" />} />
       </Routes>
     </BrowserRouter>
